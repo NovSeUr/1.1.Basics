7d93060035b5e4673a6dce182d18e8ef19f88b12
Nov.s <novikov.su20@gmail.com>
2023-04-28 23:24:46 +0500

    Add Autotests and change on JUnit5

--- src/test/java/ru/netology/cashback/CashBackHackServiceTest.java
+++ src/test/java/ru/netology/cashback/CashBackHackServiceTest.java
@@ -1,52 +1,84 @@
 package ru.netology.cashback;
 
-import org.junit.Test;
-
 import org.junit.Assert;
 
 public class CashBackHackServiceTest {
     CashBackHackService service = new CashBackHackService();
 
-    @Test
+    @org.junit.Test
     public void testShouldUseRemainCriticalPath() {
        Assert.assertEquals(service.remain(900), 100);
     }
 
-    @Test
+    @org.junit.Test
     public void testBoundaryValueOne() {
         Assert.assertThrows(RuntimeException.class, () ->{
             service.remain(-1);
         });
     }
-    @Test
+    @org.junit.Test
     public void testBoundaryValueTwo() {
         Assert.assertEquals(service.remain(0), 1000);
     }
 
-    @Test
+    @org.junit.Test
     public void testBoundaryValueThree() {
         Assert.assertEquals(service.remain(1), 999);
     }
 
-    @Test
+    @org.junit.Test
     public void testBoundaryValueFour () {
         Assert.assertEquals(service.remain(2), 998);
     }
 
-    @Test
+    @org.junit.Test
     public void testBoundaryValueFive () {
         Assert.assertEquals(service.remain(1000), 0);
     }
 
-    @Test
+    @org.junit.Test
     public void testBoundaryValueSix () {
         Assert.assertEquals(service.remain(1001), 999);
     }
 
-    @Test
+    @org.junit.Test
     public void testBoundaryValueSeven () {
         Assert.assertEquals(service.remain(999), 1);
     }
 
+    @org.junit.jupiter.api.Test
+    public void testShouldUseRemainCriticalPathJupiter() {
+        org.junit.jupiter.api.Assertions.assertEquals(600, service.remain(400));
+    }
 
+    @org.junit.jupiter.api.Test
+    public void testBoundaryValueOneJupiter() {
+        org.junit.jupiter.api.Assertions.assertThrows(RuntimeException.class, () -> {
+            service.remain(-1);
+        });
+    }
+    @org.junit.jupiter.api.Test
+    public void testBoundaryValueTwoJupiter() {
+        org.junit.jupiter.api.Assertions.assertEquals(1000, service.remain(0));
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testBoundaryValueThreeJupiter() {
+        org.junit.jupiter.api.Assertions.assertEquals(999, service.remain(1));
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testBoundaryValueFourJupiter() {
+        org.junit.jupiter.api.Assertions.assertEquals(1, service.remain(999));
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testBoundaryValueFiveJupiter() {
+        org.junit.jupiter.api.Assertions.assertEquals(0, service.remain(1000));
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testBoundaryValueSixJupiter() {
+        org.junit.jupiter.api.Assertions.assertEquals(999, service.remain(1001));
+    }
 }
