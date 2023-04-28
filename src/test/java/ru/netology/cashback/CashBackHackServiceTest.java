ef2df94e350b5b3ae9c82d214160e18c9c378a13
Nov.s <novikov.su20@gmail.com>
2023-04-28 22:29:24 +0500

    Add Autotests and change on JUnit4

--- src/test/java/ru/netology/cashback/CashBackHackServiceTest.java
+++ src/test/java/ru/netology/cashback/CashBackHackServiceTest.java
@@ -1,51 +1,51 @@
 package ru.netology.cashback;
 
-import org.testng.annotations.Test;
+import org.junit.Test;
 
-import static org.testng.Assert.*;
+import org.junit.Assert;
 
 public class CashBackHackServiceTest {
     CashBackHackService service = new CashBackHackService();
 
     @Test
     public void testShouldUseRemainCriticalPath() {
-        assertEquals(service.remain(900), 100);
+       Assert.assertEquals(service.remain(900), 100);
     }
 
     @Test
     public void testBoundaryValueOne() {
-        assertThrows(RuntimeException.class, () ->{
+        Assert.assertThrows(RuntimeException.class, () ->{
             service.remain(-1);
         });
     }
     @Test
     public void testBoundaryValueTwo() {
-        assertEquals(service.remain(0), 1000);
+        Assert.assertEquals(service.remain(0), 1000);
     }
 
     @Test
     public void testBoundaryValueThree() {
-        assertEquals(service.remain(1), 999);
+        Assert.assertEquals(service.remain(1), 999);
     }
 
     @Test
     public void testBoundaryValueFour () {
-        assertEquals(service.remain(2), 998);
+        Assert.assertEquals(service.remain(2), 998);
     }
 
     @Test
     public void testBoundaryValueFive () {
-        assertEquals(service.remain(1000), 0);
+        Assert.assertEquals(service.remain(1000), 0);
     }
 
     @Test
     public void testBoundaryValueSix () {
-        assertEquals(service.remain(1001), 999);
+        Assert.assertEquals(service.remain(1001), 999);
     }
 
     @Test
     public void testBoundaryValueSeven () {
-        assertEquals(service.remain(999), 1);
+        Assert.assertEquals(service.remain(999), 1);
     }
 
 
